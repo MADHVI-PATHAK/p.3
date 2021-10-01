cb39771cdf74cf7483f559ac43735c782db116f0
MADHVI-PATHAK <81677802+MADHVI-PATHAK@users.noreply.github.com>
2021-10-01 15:26:26 +0530

    add initial p.3 files

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const navSlide = () =>{
+      const bars = document.querySelector('.fa-bars');
+      const web = document.querySelector('.web');
+
+      bars.addEventListener('click', () => {
+          web.classList.toggle('web-active');
+      });
+  }
+navSlide();
+
